02fc6f40b92d80e7dbdb720ef556ade78ea9b59f
Максим Помогаев <79501438964@yandex.ru>
2024-11-13 22:45:30 +0700

    first layouts pages and bd updates

---
+++ FandomWiki/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FandomWiki.Database
+{
+    public class DatabaseContext: DbContext {
+
+        DbSet<Community> Community { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options){}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=FandomWiki;Username=postgres;Password=root");
+        }
+    }
+}
--- FandomWiki/Program.cs
+++ FandomWiki/Program.cs
@@ -1,7 +1,11 @@
+using FandomWiki.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql());
 
 var app = builder.Build();
 
